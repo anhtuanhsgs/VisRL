eeb030700bf381380bdcac9d922adc687fa0b31d
anhtuanhsgs <anhtuanhsgs@gmail.com>
2020-11-30 16:19:11 +0700

    up

--- run_scrips/train_debug.sh
+++ run_scrips/train_debug.sh
@@ -6,5 +6,5 @@ python main.py --env DEBUG \
 --num-steps 3 --max-episode-length 3 \
 --size 32 32 \
 --feats 64 64 128 128 1024 \
---save-period 100 --log-period 100 --train-log-period 100\
+--save-period 100 --log-period 100 --train-log-period 100 \
 --log-dir logs/Dec2020/ --save-model-dir logs/trained_models/Dec2020/ \
