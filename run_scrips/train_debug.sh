3d102e93d6d74edbc98dffb4aa6994547bc9a12c
anhtuanhsgs <anhtuanhsgs@gmail.com>
2020-11-30 16:24:41 +0700

    up

--- run_scrips/train_debug.sh
+++ run_scrips/train_debug.sh
@@ -2,7 +2,7 @@ cat $0
 python main.py --env DEBUG \
 --gpu-id 0 1 2 3 --workers 3 --valid-gpu 0 \
 --data Random \
---lr 1e-5 \
+--lr 1e-6 \
 --num-steps 3 --max-episode-length 3 \
 --size 32 32 \
 --feats 64 64 128 128 1024 \
