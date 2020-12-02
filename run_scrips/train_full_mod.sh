291cc51236b55daea9a7b2db95c34852c8f8e4b8
tuan <anhtuanhsgs@gmail.com>
2020-12-02 07:14:22 +0000

    extend RGB Aug

--- run_scrips/train_full_mod.sh
+++ run_scrips/train_full_mod.sh
@@ -3,7 +3,7 @@ python main.py --env n2RGB_FixedModAug \
 --gpu-id 0 1 2 3 --workers 8 --valid-gpu 0 \
 --data Random \
 --lr 1e-4 --num-actions 8 \
---num-steps 4 --max-episode-length 4 \
+--num-steps 12 --max-episode-length 12 \
 --size 32 32 \
 --feats 64 64 128 128 512 \
 --save-period 100 --log-period 5 --train-log-period 100 \
