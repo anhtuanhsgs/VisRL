16acf310bf34872cb5298913822dccf5154c9caa
anhtuanhsgs <anhtuanhsgs@gmail.com>
2020-12-03 12:19:05 +0700

    Extend full RGB

--- test.py
+++ test.py
@@ -56,7 +56,7 @@ def test_func (args, shared_model, env_conf, datasets):
         logger = Logger (args.log_dir)
 
         saved_src_dir = args.log_dir + "/src/"
-        create_dir (saved_code)
+        create_dir (saved_src_dir)
         os.system ("cp *.py " + saved_src_dir)
         os.system ("cp *.sh " + saved_src_dir)
         os.system ("cp -r models " + saved_src_dir)
