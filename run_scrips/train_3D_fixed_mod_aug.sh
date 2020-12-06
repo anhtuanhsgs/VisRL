a064f3aa003734e5764b9d60b78b2e456c2ddc75
anhtuanhsgs <anhtuanhsgs@gmail.com>
2020-12-06 23:34:47 +0700

    extend 3D

--- run_scrips/train_3D_fixed_mod_aug.sh
+++ run_scrips/train_3D_fixed_mod_aug.sh
@@ -3,7 +3,7 @@ python main.py --env 3D_1rgba_FixedMod \
 --gpu-id 0 1 2 3 --workers 12 --valid-gpu 0 \
 --data 3DVols \
 --lr 1e-4 \
---num-actions 4 --color-step 16 \
+--num-actions 4 --color-step 32 \
 --num-steps 4 --max-episode-length 4 \
 --size 32 32 \
 --feats 64 64 128 128 512 \
