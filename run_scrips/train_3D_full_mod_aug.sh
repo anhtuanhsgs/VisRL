0ac9d6bc506632c741d7e9f920fd45d40e17ddf0
anhtuanhsgs <anhtuanhsgs@gmail.com>
2020-12-08 11:00:30 +0700

    extend to 3D

--- run_scrips/train_3D_full_mod_aug.sh
+++ run_scrips/train_3D_full_mod_aug.sh
@@ -1,10 +1,10 @@
 cat $0
-python main.py --env 3D_1rgba_LimitedModAug \
+python main.py --env 3D_1rgba_FullModAug \
 --gpu-id 0 1 2 3 --workers 12 --valid-gpu 0 \
 --data 3DVols \
 --lr 1e-4 \
 --num-actions 4 --color-step 32 \
---num-steps 4 --max-episode-length 4 \
+--num-steps 8 --max-episode-length 8 \
 --size 32 32 \
 --feats 64 64 128 128 512 \
 --save-period 100 --log-period 10 --train-log-period 100 \
