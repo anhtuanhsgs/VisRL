981b64e7e4afdcd0543a9d01f9184fb997530c08
anhtuanhsgs <anhtuanhsgs@gmail.com>
2020-12-08 17:58:00 +0700

    extend to 3D

--- run_scrips/train_3D_limited_mod_limited_aug.sh
+++ run_scrips/train_3D_limited_mod_limited_aug.sh
@@ -3,8 +3,8 @@ python main.py --env 3D_1rgba_LimitedModLimitedAug \
 --gpu-id 0 1 2 3 --workers 12 --valid-gpu 0 \
 --data 3DVols \
 --lr 1e-4 \
---num-actions 4 --color-step 16 \
---num-steps 6 --max-episode-length 6 \
+--num-actions 4 --color-step 32 \
+--num-steps 4 --max-episode-length 4 \
 --size 32 32 \
 --feats 64 64 128 128 512 \
 --save-period 100 --log-period 10 --train-log-period 100 \
--- run_scrips/train_chest_limited_mod_limited_aug.sh
+++ run_scrips/train_chest_limited_mod_limited_aug.sh
@@ -1,10 +1,10 @@
 cat $0
 python main.py --env chest_1rgba_LimitedModLimitedAug \
---gpu-id 0 1 2 3 --workers 12 --valid-gpu 0 \
+--gpu-id 0 1 2 3 --workers 16 --valid-gpu 0 \
 --data 3DChest \
 --lr 1e-4 \
---num-actions 4 --color-step 16 \
---num-steps 5 --max-episode-length 5 \
+--num-actions 7 --color-step 16 \
+--num-steps 4 --max-episode-length 4 \
 --size 128 128 \
 --feats 64 64 128 512 1024 \
 --save-period 100 --log-period 10 --train-log-period 100 \
