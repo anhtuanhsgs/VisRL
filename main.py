94671b25890cb99d8a713675b5805def3d50bb1c
anhtuanhsgs <anhtuanhsgs@gmail.com>
2020-12-08 15:05:03 +0700

    extend to 3D

--- main.py
+++ main.py
@@ -236,6 +236,7 @@ def setup_data (args, set_type):
     if args.data == "3DChest":
         raw = read_imgs_from_path ("Data/Medical/" + set_type + "/A/")
         raw = [ vol [::2, ::2, ::2] for vol in raw ]
+        print (len (raw))
         datasets = [raw]
         args.data_channel = 1
         args.lut_init = [0, 0, 0, 0, 10, 50, 50]
