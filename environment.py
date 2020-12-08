2fd9f8d025073dbfa56bc7ebfb306d0b5377af9d
anhtuanhsgs <anhtuanhsgs@gmail.com>
2020-12-08 14:16:51 +0700

    extend to 3D

--- environment.py
+++ environment.py
@@ -130,8 +130,8 @@ class Debug_env (General_env):
         self.ref, self.raw = self.aug3D ([self.ref, self.raw])
         self.ref = self.aug3D ([self.ref]) [0]
 
-        self.lut = LUT (rng=self.rng, n=self.num_actions, color_step=self.color_step, is3D=self.is3D, self.lut_init)
-        self.ref_lut = LUT (rng=self.rng, n=self.num_actions, color_step=self.color_step, is3D=self.is3D, self.lut_init)
+        self.lut = LUT (rng=self.rng, n=self.num_actions, color_step=self.color_step, is3D=self.is3D, initial=self.lut_init)
+        self.ref_lut = LUT (rng=self.rng, n=self.num_actions, color_step=self.color_step, is3D=self.is3D, initial=self.lut_init)
 
         self.ref_lut.rand_mod ()
 
